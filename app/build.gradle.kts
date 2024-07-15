bea8e36db65922699813d740d2a46f266e170078
tuuhin <tuhinbhowmick2513@gmail.com>
2024-07-15 12:05:22 +0530

    Added some dependencies Dependencies which will be later used in the porject are added, Added hilt to be used for DI purpose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.jetbrains.kotlin.android)
 	alias(libs.plugins.compose.compiler)
+	alias(libs.plugins.ksp)
+	alias(libs.plugins.hilt)
 }
 
 android {
@@ -65,6 +67,17 @@ dependencies {
 	implementation(libs.androidx.ui.graphics)
 	implementation(libs.androidx.ui.tooling.preview)
 	implementation(libs.androidx.material3)
+	//lifecycle compose
+	implementation(libs.androidx.lifecycle.runtime.compose)
+	//kotlinx-datetime
+	implementation(libs.kotlinx.datetime)
+	//hilt
+	ksp(libs.hilt.android.compiler)
+	implementation(libs.hilt.android)
+	//media3
+	implementation(libs.androidx.media3.common)
+	//splash
+	implementation(libs.androidx.core.splashscreen)
 	// tests
 	testImplementation(libs.junit)
 	//android tests
---
+++ app/src/main/java/com/eva/recorderapp/RecorderApp.kt
@@ -0,0 +1,7 @@
+package com.eva.recorderapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RecorderApp : Application()
