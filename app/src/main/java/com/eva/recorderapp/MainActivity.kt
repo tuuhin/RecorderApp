ad3a0023f5f1b5f8bf9f94b77900317d41f111f6
tuuhin <tuhinbhowmick2513@gmail.com>
2025-04-14 02:42:31 +0530

    A bit of animation with the splash and shortcut icon color change. Shortcut icons colors now follow the android dynamic colors. As per the splash screen an object animator was added with drawable. splash api onExitAnimationListener removing the icon view with some animation again.

--- app/src/main/java/com/eva/recorderapp/MainActivity.kt
+++ app/src/main/java/com/eva/recorderapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.eva.recorderapp
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -20,9 +26,13 @@ class MainActivity : ComponentActivity() {
 	private lateinit var navController: NavHostController
 
 	override fun onCreate(savedInstanceState: Bundle?) {
-		installSplashScreen()
+
+		// configure splash screen
+		configureSplashScreen(onAnimationEnd = { enableEdgeToEdge() })
+
 		super.onCreate(savedInstanceState)
 
+		// if enabled edge to edge not applied after animation
 		enableEdgeToEdge()
 
 		setContent {
@@ -40,5 +50,45 @@ class MainActivity : ComponentActivity() {
 	override fun onNewIntent(intent: Intent) {
 		super.onNewIntent(intent)
 		navController.handleDeepLink(intent)
+	}
+}
+
+private fun Activity.configureSplashScreen(onAnimationEnd: () -> Unit = {}) {
+	val splash = installSplashScreen()
+
+	splash.setOnExitAnimationListener { screenView ->
+		// do all the animation is reverse way
+		val interpolator = AccelerateDecelerateInterpolator()
+		val duration = 1000L
+
+		val scaleXAnimation = ObjectAnimator
+			.ofFloat(screenView.iconView, View.SCALE_X, 1f, 0.5f)
+			.apply {
+				this.interpolator = interpolator
+				this.duration = duration
+			}
+
+		val scaleYAnimation = ObjectAnimator
+			.ofFloat(screenView.iconView, View.SCALE_Y, 1f, 0.5f)
+			.apply {
+				this.interpolator = interpolator
+				this.duration = duration
+			}
+
+		val translateAnimation = ObjectAnimator
+			.ofFloat(screenView.iconView, View.TRANSLATION_Y, 0.0f, 20.0f)
+			.apply {
+				this.interpolator = interpolator
+				this.duration = duration
+			}
+
+		val animatorSet = AnimatorSet().apply {
+			playTogether(scaleXAnimation, scaleYAnimation, translateAnimation)
+			doOnEnd {
+				screenView.remove()
+				onAnimationEnd()
+			}
+		}
+		animatorSet.start()
 	}
 }
