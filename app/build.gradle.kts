2facde0b6b97c613a33dd798f8263c2f7fa5d97d
tuuhin <tuhinbhowmick2513@gmail.com>
2025-05-13 13:49:42 +0530

    Adding CI pipeline and update version no. Adding module graph dependency which was missed due to merge conflict. Update the version code to 8 and version 1.3.0 as this adds a new feature thus a minor new version release. Mistakenly merge the branch in main containing build failure thus adding a CI pipeline to ensure there is no build failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 		applicationId = "com.eva.recorderapp"
 		minSdk = 29
 		targetSdk = 35
-		versionCode = 7
-		versionName = "1.2.2"
+		versionCode = 8
+		versionName = "1.3.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
