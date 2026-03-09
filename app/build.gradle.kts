3a7a25307c84d8bd17239488fedd70f06cfe70ae
tuuhin <tuhinbhowmick2513@gmail.com>
2026-03-09 23:00:29 +0530

    Creating a beta release This beta release will contain live translation logic from feature/transcribe branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 		applicationId = "com.eva.recorderapp"
 		minSdk = libs.versions.minSdk.get().toInt()
 		targetSdk = libs.versions.compileSdk.get().toInt()
-		versionCode = 14
-		versionName = "1.4.4"
+		versionCode = 15
+		versionName = "1.5.0-beta1"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
